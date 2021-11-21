395c5b3071bf34324e3f9da48464c5946a31bc5b
magnai-coder <61421435+magnai-coder@users.noreply.github.com>
2021-11-21 22:31:40 +0800

    Create GPA formula.cpp

---
+++ GPA formula.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(void)
+ {  
+     long n, a[n], i;
+	 float s, m; 
+  	 s=0;
+ 	 printf("Dungiin toogoo oruulna uu\n"); scanf("%d",&n);
+ 	 for(i=0; i<n; i++) {
+ 	 	printf("Hicheel dugaar \t%d dun=", 1+i); scanf("%d",&a[i]);
+ 	 	while(a[i]>100){ 
+			printf("Aldaatai utga bna 1-100 hoorond unelne uu\n");
+			scanf("%d",&a[i]);
+		    }
+           	printf("\n");
+	      }
+	      printf("hello");
+		
+	  printf("%7s%13s\n","Hicheel","Dun");
+	  for(i=0; i<n; i++){
+	  	printf("%7d%13d\n",i+1, a[i]);
+	  	s=s+a[i]*4/100;
+	  }
+	  s=s/n;
+	  printf("Golch dun-%.1f", s);
+	  return 0;
+ }
+
